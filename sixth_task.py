8d8051d28ad9cbb4bc2468cccdd0c8de1d0b2478
nanostarlee <nanostarleelawrence@gmail.com>
2022-09-28 23:44:05 +0300

    Game class with some its methods as per task given

---
+++ sixth_task.py
@@ -0,0 +1,80 @@
+import pygame
+import time
+from pygame.locals import *
+
+SIZE = 40
+BACKGROUND_COLOR = (110, 110, 5)
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Toyota Snake Game")
+
+        pygame.mixer.init()
+        self.play_background_music()
+        self.surface = pygame.display.set_mode((1000, 800))
+        self.snake = Snake(self.surface)
+        self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def play_background_music(self):
+        pygame.mixer.music.load('resources/bg_music.mp3')
+        pygame.mixer.music.play(-1, 0)
+
+    def play_sound(self, sound_name):
+        if sound_name == "crash":
+            sound = pygame.mixer.Sound("resources/crash.wav")
+        elif sound_name == 'ding':
+            sound = pygame.mixer.Sound("resources/Ding.mp3")
+
+        pygame.mixer.Sound.play(sound)
+        # pygame.mixer.music.stop()
+
+        self.surface = pygame.display.set_mode((1000, 8000))
+
+
+    def run(self):
+        running = True
+        pause = False
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                    if event.key == K_RETURN:
+                        pygame.mixer.music.unpause()
+                        pause = False
+
+                    if not pause:
+                        if event.key == K_LEFT:
+                            self.snake.move_left()
+
+                        if event.key == K_RIGHT:
+                            self.snake.move_right()
+
+                        if event.key == K_UP:
+                            self.snake.move_up()
+
+                        if event.key == K_DOWN:
+                            self.snake.move_down()
+
+                elif event.type == QUIT:
+                    running = False
+            try:
+
+                if not pause:
+                    self.play()
+
+            except Exception as e:
+                self.show_game_over()
+                pause = True
+                self.reset()
+
+            time.sleep(.1)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
